1e3962247ebec6c9b54a427a6945244707ac7c5a
Jonathan Conder <jonno.conder@gmail.com>
2018-04-07 11:07:46 -0700

    Remove old files

--- xmonad.hs
+++
@@ -1,8 +0,0 @@
-import XMonad
-import XMonad.Hooks.EwmhDesktops
-
-main = xmonad defaultConfig
-    { borderWidth     = 0
-    , terminal        = "lxterminal"
-    , handleEventHook = handleEventHook defaultConfig <+> fullscreenEventHook
-    }
